13d353e1010a7ef7534320a148d3dbbd88b0dcaa
moelt-mb <mmark5437@yahoo.com>
2023-07-17 12:27:01 -0700

    init

---
+++ app/Entry.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useState } from "react"
+
+export default function Entry() {
+    const [count, setCount] = useState("");
+
+    return (
+        <div>
+            <h1>Title</h1>            
+        </div>
+    )
+}
